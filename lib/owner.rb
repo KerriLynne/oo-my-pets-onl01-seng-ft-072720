d49b72ed0d320ad9e03b9f54d2bc2a92d601666b
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-11 17:49:26 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -7,8 +7,13 @@ class Owner
   def initialize(name)
     @name = name
     @species = "human"
-
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+    
 
   def say_species
     "I am a #{@species}."
